9cc307735b3759e3551feeb61431254b7247fdd1
杨明 <yangming@testin.cn>
2017-03-30 15:31:53 +0800

    添加json文件依赖，以便watch

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function getValue(str){
   return fn();
 }
 //提取 {{>targetName options}} 中的options数据
-function custom_data(str,dirname){
+function custom_data(str,file){
   var data = {};
   str.replace(/(\{\{>\w+\s*)|(\}\}$)/g,'').split(/\s+/).map(function(item){
     var arr = item.split('=');
@@ -23,8 +23,12 @@ function custom_data(str,dirname){
     if(arr.length<2)return;
     if(arr[1].indexOf('.json')>0){
       var jsonPath = arr[1].replace(/^"|"$/g,'');
-      data = require(path.join(jsonPath.indexOf('/')===0?fis.project.getProjectPath()
-      :dirname,jsonPath));
+      var filepath = path.join(jsonPath.indexOf('/')===0?fis.project.getProjectPath()
+      :file.dirname,jsonPath);
+      data = JSON.parse(fis.file(filepath).getContent());
+      if(file.cache&&file.cache.addDeps){
+        file.cache.addDeps(filepath);
+      }
     }else{
       data[arr[0]] = getValue(arr[1]);
     }
@@ -36,13 +40,23 @@ var handles = function(content,file,options){
       opts = Object.assign({},def,options);
     }
     let name = file.fullname.replace(file.dirname+'/','').replace(file.ext,'');
+    if (file.useCache&&!file.cache) {
+      // 如果启用，则创建 cache 对象
+      file.cache = fis.cache(file.realpath, fis.project.getCachePath());
+      // 保存内容以及信息到缓存中。
+      file.cache.save();
+    }
     //从内容中提取 partials 并进行内容替换
     var content = content.replace(/\{\{>([\w\/\.]+)(\s*[^\}\s])*\}\}/g,function(a,b){
       var body = '';
       if(!opts.partials[b]){
         var filepath = path.join(b.indexOf('/')===0?fis.project.getProjectPath():file.dirname,b+opts.extname);
-        var fl = fis.file(filepath);
-        body = handles(fl.getContent().toString(fl.charset),fl,custom_data(arguments[0],file.dirname));
+        var fl = fis.file(filepath),
+        _cdata = custom_data(arguments[0],file.dirname);
+        body = handles(fl.getContent().toString(fl.charset),fl,custom_data(arguments[0],file));
+      }
+      if(body&&file.cache&&file.cache.addDeps){
+        file.cache.addDeps(file.fullname);
       }
       return body||'';
     });
@@ -51,8 +65,11 @@ var handles = function(content,file,options){
     let jsonData = {};
     //如果json存在并且有值才进行保存
     if(fis.file(dataFilePath).exists()&&fis.util.isFile(dataFilePath)){
-      var json = require(dataFilePath);
+      var json = JSON.parse(fis.file(dataFilePath).getContent());
       fis.util.isEmpty(json)||(jsonData=json);
+      if(file.cache&&file.cache.addDeps){
+        file.cache.addDeps(dataFilePath);
+      }
     }
     let data = Object.assign({},jsonData,options||{});
     let tpl = hbs.compile(content);
